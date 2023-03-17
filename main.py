a2f3a93354f582ae03030633bc7f9ab36c8a4cd9
Haffi Mazhar <haffimazhar96@gmail.com>
2023-03-17 16:59:08 +0000

    test poetry cache

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def hello() -> JSONResponse:
+    return JSONResponse({"Message": "Hello"}, status_code=200)
